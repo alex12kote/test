1e1594c8fcb50437aef07db7df29e2b44dc6674c
session-0 <alex12kote@gmail.com>
2018-09-29 23:28:09 +0300

    go

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const arr = ['hello', 'world', 'test'];
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  res.render('index', { arr: arr });
 });
-
-app.listen(3000, function () {
+app.get('/create', function(req, res) {
+  res.render('create');
+});
+app.post('/create', function(req, res) {
+  arr.push(req.body.text);
+  res.redirect('/');
+});
+app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
